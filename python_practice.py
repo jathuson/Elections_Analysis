d8ac2196ff9f131ce2bf2395c1e1aded954dcf9f
Jathuson <jathusonj@gmail.com>
2021-11-20 12:43:41 -0500

    Adding PyPoll.py file.

---
+++ python_practice.py
@@ -0,0 +1,47 @@
+# voting_data = []
+# voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+# voting_data.append({"county":"Denver", "registered_voters": 463353})
+# voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+# voting_data.append({"county":"El Paso", "registered_voters": 461149})
+
+
+# How many votes did you get?
+#my_votes = int(input("How many votes did you get in the election? "))
+#  Total votes in the election
+# total_votes = int(input("What is the total votes in the election? "))
+# # Calculate the percentage of votes you received.
+# percentage_votes = (my_votes / total_votes) * 100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
+
+# for county in counties_dict:
+#     print(county)
+
+# for county, voters in counties_dict.items():
+#     print(county, voters)
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    print(county_dict['county'])
+
+# Open the data file.
+# Write down the names of all the candidates.
+# Add a vote count for each candidate.
+# Get the total votes for each candidate.
+# Get the total votes cast for the election.
+
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of candidates who recieved votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
