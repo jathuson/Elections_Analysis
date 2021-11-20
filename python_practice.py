ff36c4dea6ad7d0f3126db58d88523b71011eb04
Jathuson <jathusonj@gmail.com>
2021-11-20 13:11:50 -0500

    Adding the election audit code

--- python_practice.py
+++ python_practice.py
@@ -45,3 +45,17 @@ for county_dict in voting_data:
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
+
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Create a filename variable to a direct or indirect path to the file.
+    file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Counties in the Election\n------------------------\nArapahoe\nDenver\nJefferson")
